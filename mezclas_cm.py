bb39e5f28421f2501cd07eb74429dd880f7483be
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2023-03-13 20:11:57 -0300

    Correciones esteticas menores. Se agrego capacidad de usar comas decimales, ademas del punto

--- mezclas_cm.py
+++ mezclas_cm.py
@@ -12,19 +12,19 @@ class MezclaCm(ttk.Frame):
       self.mezcla = Mezcla(0, 0, 0, 0, 0, 0)
 
       ttk.Label(self, text='Tanque').grid(column=2, row=1, sticky=W)
-      self.tanque_combo = ttk.Combobox(self, width=40, state='readonly', values=['19', '28'])
+      self.tanque_combo = ttk.Combobox(self, width=40, state='readonly', values=['19', '28'], justify=tk.RIGHT)
       self.tanque_combo.grid(column=3, row=1, sticky=W)      
       ttk.Label(self, text='Medida inicial').grid(column=2, row=2, sticky=W)
       self.medida_inicial = StringVar()
-      self.medida_inicial_entry = ttk.Entry(self, width=20, textvariable=self.medida_inicial)
+      self.medida_inicial_entry = ttk.Entry(self, width=20, textvariable=self.medida_inicial, justify=tk.RIGHT)
       self.medida_inicial_entry.grid(column=3, row=2, sticky=(W, E))
       ttk.Label(self, text='Concentracion inicial').grid(column=2, row=3, sticky=W)
       self.conc_inicial = StringVar()
-      self.conc_inicial_entry = ttk.Entry(self, width=20, textvariable=self.conc_inicial)
+      self.conc_inicial_entry = ttk.Entry(self, width=20, textvariable=self.conc_inicial, justify=tk.RIGHT)
       self.conc_inicial_entry.grid(column=3, row=3, sticky=(W, E))
       self.mf_label = ttk.Label(self, text='Medida final').grid(column=2, row=4, sticky=W)
       self.medida_final = StringVar()
-      self.medida_final_entry = ttk.Entry(self, width=20, textvariable=self.medida_final)
+      self.medida_final_entry = ttk.Entry(self, width=20, textvariable=self.medida_final, justify=tk.RIGHT)
       self.medida_final_entry.grid(column=3, row=4, sticky=(W, E))
       ttk.Label(self, text='Cantidad de mezclas').grid(column=2, row=5, sticky=W)
       self.radio_frame = Frame(self)
@@ -41,28 +41,29 @@ class MezclaCm(ttk.Frame):
       ttk.Label(self, text='Concentracion deseada').grid(column=2, row=6, sticky=W)
       self.conc_deseada = StringVar()
       self.conc_deseada.set('65.5')
-      self.conc_deseada_entry = ttk.Entry(self, width=20, textvariable=self.conc_deseada)
+      self.conc_deseada_entry = ttk.Entry(self, width=20, textvariable=self.conc_deseada, justify=tk.RIGHT)
       self.conc_deseada_entry.grid(column=3, row=6, sticky=(W, E))
 
       ttk.Label(self, text='Concentracion ácido concentrado').grid(column=2, row=7, sticky=W)
 
       self.conc_concentrado = StringVar()
-      self.conc_concentrado_entry = ttk.Entry(self, width=20, textvariable=self.conc_concentrado)
+      self.conc_concentrado_entry = ttk.Entry(self, width=20, textvariable=self.conc_concentrado, justify=tk.RIGHT)
       self.conc_concentrado_entry.grid(column=3, row=7, sticky=(W, E))
 
       ttk.Label(self, text='Concentracion ácido diluido').grid(column=2, row=8, sticky=W)
 
       self.conc_diluido = StringVar()
-      self.conc_diluido_entry = ttk.Entry(self, width=20, textvariable=self.conc_diluido)
+      self.conc_diluido_entry = ttk.Entry(self, width=20, textvariable=self.conc_diluido, justify=tk.RIGHT)
       self.conc_diluido_entry.grid(column=3, row=8, sticky=(W, E))
 
-      ttk.Button(self, text='Calcular', command=self.calcular).grid(column=3, row=9, sticky=E)
+      ttk.Button(self, text='Calcular', command=self.calcular, padding=(24, 12)) \
+         .grid(column=3, row=9, sticky=E)
 
       self.res = StringVar()
       ttk.Label(self, textvariable=self.res).grid(column=2, row=10, columnspan = 2, sticky=(W, E))
 
       for child in self.winfo_children():
-           child.grid_configure(padx=3, pady=5)
+           child.grid_configure(padx=5, pady=8)           
 
       self.medida_inicial_entry.focus()
       self.bind("<Return>", self.calcular)
@@ -70,19 +71,15 @@ class MezclaCm(ttk.Frame):
    def cantidad_mezclas(self):
     if self.cantidad.get() == 1:
        self.medida_final.set('150')
-       self.mezcla.tanque = Tanque(28, 178.7)
        self.tanque_combo.set('28')
     elif self.cantidad.get() == 2:
        self.medida_final.set('250')
-       self.mezcla.tanque = Tanque(28, 178.7)
        self.tanque_combo.set('28')
     elif self.cantidad.get() == 3:
        self.medida_final.set('365')  
-       self.mezcla.tanque = Tanque(28, 178.7)
        self.tanque_combo.set('28')
     else:
        self.medida_final.set('295')  
-       self.mezcla.tanque = Tanque(19, 68)
        self.tanque_combo.set('19')
 
    def calcular(self):
@@ -91,7 +88,8 @@ class MezclaCm(ttk.Frame):
          self.mezcla.tanque = Tanque(19, 68.0)
       else: 
          self.mezcla.tanque = Tanque(28, 178.7)
-
+      
+      self.__format()
       self.mezcla.medida_inicial = float(self.medida_inicial.get())
       self.mezcla.concentracion_inicial = float(self.conc_inicial.get())
       self.mezcla.medida_final = float(self.medida_final.get())
@@ -103,3 +101,13 @@ class MezclaCm(ttk.Frame):
 
       self.res.set(f'Llevar a {cm_dil}cm con diluido y completar a {cm_conc}cm con concentrado')
 
+   def __format(self):
+      self.medida_inicial.set(self.__cambiar_coma(self.medida_inicial.get()))
+      self.conc_inicial.set(self.__cambiar_coma(self.conc_inicial.get()))
+      self.medida_final.set(self.__cambiar_coma(self.medida_final.get()))
+      self.conc_deseada.set(self.__cambiar_coma(self.conc_deseada.get()))
+      self.conc_concentrado.set(self.__cambiar_coma(self.conc_concentrado.get()))
+      self.conc_diluido.set(self.__cambiar_coma(self.conc_diluido.get()))
+
+   def __cambiar_coma(self, value):
+      return value.replace(',', '.')   
--- mezclas_corregir.py
+++ mezclas_corregir.py
@@ -12,37 +12,37 @@ class MezclaCorregir(ttk.Frame):
       self.mezcla = Mezcla(0, 0, 0, 0, 0, 0)    
 
       ttk.Label(self, text='Tanque').grid(column=2, row=1, sticky=W)
-      self.tanque_combo = ttk.Combobox(self, width=40, state='readonly', values=['19', '28'])
+      self.tanque_combo = ttk.Combobox(self, width=40, state='readonly', values=['19', '28'], justify=tk.RIGHT)
       self.tanque_combo.grid(column=3, row=1, sticky=W)  
 
       ttk.Label(self, text='Medida inicial').grid(column=2, row=2, sticky=W)
       self.medida_inicial = StringVar()
-      self.medida_inicial_entry = ttk.Entry(self, width=20, textvariable=self.medida_inicial)
+      self.medida_inicial_entry = ttk.Entry(self, width=20, textvariable=self.medida_inicial, justify=tk.RIGHT)
       self.medida_inicial_entry.grid(column=3, row=2, sticky=(W, E))
 
       ttk.Label(self, text='Concentracion inicial').grid(column=2, row=3, sticky=W)
       self.conc_inicial = StringVar()
-      self.conc_inicial_entry = ttk.Entry(self, width=20, textvariable=self.conc_inicial)
+      self.conc_inicial_entry = ttk.Entry(self, width=20, textvariable=self.conc_inicial, justify=tk.RIGHT)
       self.conc_inicial_entry.grid(column=3, row=3, sticky=(W, E))
 
       ttk.Label(self, text='Concentracion deseada').grid(column=2, row=4, sticky=W)
       self.conc_deseada = StringVar()
       self.conc_deseada.set('65.5')
-      self.conc_deseada_entry = ttk.Entry(self, width=20, textvariable=self.conc_deseada)
+      self.conc_deseada_entry = ttk.Entry(self, width=20, textvariable=self.conc_deseada, justify=tk.RIGHT)
       self.conc_deseada_entry.grid(column=3, row=4, sticky=(W, E))
 
       ttk.Label(self, text='Concentracion ácido conc / dil').grid(column=2, row=5, sticky=W)
       self.conc_concentrado = StringVar()
-      self.conc_concentrado_entry = ttk.Entry(self, width=20, textvariable=self.conc_concentrado)
+      self.conc_concentrado_entry = ttk.Entry(self, width=20, textvariable=self.conc_concentrado, justify=tk.RIGHT)
       self.conc_concentrado_entry.grid(column=3, row=5, sticky=(W, E))        
 
-      ttk.Button(self, text='Calcular', command=self.calcular).grid(column=3, row=6, sticky=E)
+      ttk.Button(self, text='Calcular', command=self.calcular, padding=(24, 12)).grid(column=3, row=6, sticky=E)
 
       self.res = StringVar()
       ttk.Label(self, textvariable=self.res).grid(column=2, row=7, columnspan = 2, sticky=(W, E))
 
       for child in self.winfo_children():
-           child.grid_configure(padx=3, pady=5)
+           child.grid_configure(padx=5, pady=8)
 
       self.medida_inicial_entry.focus()
       self.bind("<Return>", self.calcular)   
@@ -53,6 +53,8 @@ class MezclaCorregir(ttk.Frame):
          self.mezcla.tanque = Tanque(19, 68.0)
       else: 
          self.mezcla.tanque = Tanque(28, 178.7)
+
+      self.__format()   
          
       self.mezcla.medida_inicial = float(self.medida_inicial.get())
       self.mezcla.concentracion_inicial = float(self.conc_inicial.get())
@@ -70,3 +72,11 @@ class MezclaCorregir(ttk.Frame):
 
       self.res.set(f'Corregir con {round(cm_corr, 2)}cm de {producto}')
 
+   def __format(self):
+      self.medida_inicial.set(self.__cambiar_coma(self.medida_inicial.get()))
+      self.conc_inicial.set(self.__cambiar_coma(self.conc_inicial.get()))      
+      self.conc_deseada.set(self.__cambiar_coma(self.conc_deseada.get()))
+      self.conc_concentrado.set(self.__cambiar_coma(self.conc_concentrado.get()))    
+
+   def __cambiar_coma(self, value):
+      return value.replace(',', '.')
